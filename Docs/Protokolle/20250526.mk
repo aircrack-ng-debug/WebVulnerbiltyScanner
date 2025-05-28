6189760937f9f67c9426254255938f6a79150439
LearningBirdi1212 <168742561+LearningBirdi1212@users.noreply.github.com>
2025-05-28 14:46:35 +0200

    Create 20250526.mk

---
+++ Docs/Protokolle/20250526.mk
@@ -0,0 +1,6 @@
+# Protokoll 4 – 26.05.2025
+
+- Entscheidung für asynchrones Request-Framework (aiohttp)
+- Marius implementiert erste Checks (A02, A05)
+- Paul finalisiert die Ausarbeitung der Personas und Einleitung
+- Diskussion über Scope-Freeze in KW 22
